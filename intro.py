9e0092753633795a4f4d247049fd4cc09b277b96
a3malik <amit26klm@gmail.com>
2025-09-23 17:23:31 +0530

    linspace examples

--- intro.py
+++ intro.py
@@ -1,4 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-print(dir(plt))
+t=np.linspace(0, 40, 1000)
+
+d_r=2.5 *t 
+d_s=3*(t-5)
+
+fig, ax = plt.subplots()
+plt.xlabel('time (in minutes)')
+plt.ylabel('distance (in km)')
+
+ax.set_xlim(0,40)
+ax.set_ylim(0,100)
+
+ax.plot(t, d_r, c='green')
+ax.plot(t, d_s, c='brown')
+
+plt.axvline(x=30, color='purple', linestyle='--')
+plt.axhline(y=75, color='purple', linestyle='--')
+
