be976296f1204cc67d67229ebbb7acd71e51130f
a3malik <amit26klm@gmail.com>
2025-10-10 20:22:52 +0530

    moved statements to functions

--- tensors.py
+++ tensors.py
@@ -1,15 +1,17 @@
 #Scalars
-x=25
-print(type(x))
-y=3.5
-print(type(x+y))
+def ordinary_scalars():
+    x=25
+    print(type(x))
+    y=3.5
+    print(type(x+y))
 
 import torch
-x_pt=torch.tensor(25)
-print(x_pt.shape)
+def torch_scalars():
+    x_pt=torch.tensor(25)
+    print(x_pt.shape)
 
+import tensorflow as tf
 def tensorflow_scalar():
-    import tensorflow as tf
     x_tf=tf.Variable(35)
     print(type(x_tf))
     print(x_tf.shape)
@@ -23,13 +25,14 @@ def tensorflow_scalar():
 
 #Vectors
 import numpy as np
-x=np.array([25,2,5])
-print("length of x is ",len(x))
-print("shape of x is ",x.shape)
-x=np.array([[12,2,9],[3,14,7]],dtype=np.float16)
-print("Now new shape of x is ",x.shape)
-print("type of x is ",type(x))
-print("datatype of x is ",type(x.T[:,1][2]))
+def vector_prints():
+    x=np.array([25,2,5])
+    print("length of x is ",len(x))
+    print("shape of x is ",x.shape)
+    x=np.array([[12,2,9],[3,14,7]],dtype=np.float16)
+    print("Now new shape of x is ",x.shape)
+    print("type of x is ",type(x))
+    print("datatype of x is ",type(x.T[:,1][2]))
 
 #Transpose of a regular 1D array
 x=np.array([4,5,6]) #does this vector represent a point in 3D space?
@@ -41,3 +44,14 @@ print(x.T.shape)
 x=np.array([[4,5],[5,6],[6,7]])
 print(x,x.shape)
 print(x.T,x.T.shape)
+
+#How are 1D arrays different from vactors? Is 2D array a vector?
+
+def _1D_and_2D_vectors():
+    '''np.array can create a 1D as well as 2D vector, why do we call a 2D array a 2D vector?
+    for a 2D array to be a vector it should have only one column or one row'''
+    _1d_vector=np.array([22,8,4])
+    _2d_vector=np.array([[22,8,4]])
+    print(_1d_vector,_1d_vector.shape, _1d_vector.T.shape)
+    print(_2d_vector,_2d_vector.shape, _2d_vector.T.shape)
+_1D_and_2D_vectors()
